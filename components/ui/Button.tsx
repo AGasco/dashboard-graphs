3d857587f762592d31817410a5219730148609e4
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-13 21:07:11 +0000

    Implemented generic Input & Button UI components

---
+++ components/ui/Button.tsx
@@ -0,0 +1,20 @@
+import clsx from 'clsx';
+import { ButtonHTMLAttributes } from 'react';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = ({ children, className, ...props }: Props) => {
+  return (
+    <button
+      className={clsx(
+        'px-3 py-2 text-white bg-blue-500 rounded hover:bg-blue-600',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
