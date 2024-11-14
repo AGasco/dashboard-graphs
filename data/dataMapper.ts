a7a95048835daef923715aec4d970d8a7891abca
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-14 19:37:51 +0000

    Fixed a bug when parsing dates in getMappedIncidents

--- data/dataMapper.ts
+++ data/dataMapper.ts
@@ -1,21 +1,31 @@
-import { Incident, Location, IncidentType, Status } from '@/types';
 import { records } from '@/data';
+import { Incident, IncidentType, Location, Status } from '@/types';
 
 let cachedIncidents: Incident[] | null = null;
+
+const parseDate = (dateString: string): Date | null => {
+  const [day, month, year] = dateString.split('/').map(Number);
+  return !isNaN(day) && !isNaN(month) && !isNaN(year)
+    ? new Date(year, month - 1, day)
+    : null;
+};
 
 export const getMappedIncidents = (): Incident[] => {
   if (cachedIncidents) return cachedIncidents;
 
-  const mappedIncidents = records.map((record) => ({
-    ...record,
-    location: record.location as Location,
-    incident_type: record.incident_type as IncidentType,
-    status: record.status as Status,
-    date_reported: new Date(record.date_reported),
-    resolution_date: record.resolution_date
-      ? new Date(record.resolution_date)
-      : undefined
-  }));
+  const mappedIncidents = records.map(
+    (record) =>
+      ({
+        ...record,
+        location: record.location as Location,
+        incident_type: record.incident_type as IncidentType,
+        status: record.status as Status,
+        date_reported: parseDate(record.date_reported),
+        resolution_date: record.resolution_date
+          ? parseDate(record.resolution_date)
+          : undefined
+      } as Incident)
+  );
 
   cachedIncidents = mappedIncidents;
 
