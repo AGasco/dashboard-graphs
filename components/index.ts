bd6e0e5d6aa6a3989f5f030ce0160fe8115c286c
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-14 19:37:29 +0000

    Modified Select component

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export * from './ui';
 
 export { default as Navbar } from './Navbar';
 export { default as NextAuthProvider } from './NextAuthProvider';
+export { default as Pagination } from './Pagination';
--- components/ui/Select.tsx
+++ components/ui/Select.tsx
@@ -8,10 +8,11 @@ interface Option {
 interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
   label?: string;
   options: Option[];
+  defaultOptionLabel?: string;
 }
 
 const Select = forwardRef<HTMLSelectElement, Props>(
-  ({ label, options, ...props }, ref) => {
+  ({ label, options, defaultOptionLabel, ...props }, ref) => {
     return (
       <div>
         {label && <label className="block mb-1 font-semibold">{label}</label>}
@@ -20,6 +21,7 @@ const Select = forwardRef<HTMLSelectElement, Props>(
           className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
           {...props}
         >
+          {defaultOptionLabel && <option value="">{defaultOptionLabel}</option>}
           {options.map((option) => (
             <option key={option.value} value={option.value}>
               {option.label}
