7080af109bd6be118c4f58e56f7750b2c2e46ad9
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-14 19:37:11 +0000

    Pagination component

---
+++ components/Pagination.tsx
@@ -0,0 +1,56 @@
+'use client';
+import React, { useMemo } from 'react';
+import { Button } from './ui';
+
+interface Props {
+  currentPage: number;
+  totalItems: number;
+  itemsPerPage: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination = ({
+  currentPage,
+  totalItems,
+  itemsPerPage,
+  onPageChange
+}: Props) => {
+  const totalPages = useMemo(
+    () => Math.ceil(totalItems / itemsPerPage),
+    [totalItems, itemsPerPage]
+  );
+
+  const handlePrevious = () => {
+    if (currentPage > 1) onPageChange(currentPage - 1);
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) onPageChange(currentPage + 1);
+  };
+
+  if (totalPages === 0) return null;
+
+  return (
+    <div className="flex items-center justify-center mt-4 space-x-4">
+      <Button
+        onClick={handlePrevious}
+        disabled={currentPage === 1}
+        className="px-4 py-2 text-white bg-blue-500 rounded disabled:bg-gray-300"
+      >
+        Previous
+      </Button>
+      <span>
+        {currentPage} / {totalPages}
+      </span>
+      <Button
+        onClick={handleNext}
+        disabled={currentPage === totalPages}
+        className="px-4 py-2 text-white bg-blue-500 rounded disabled:bg-gray-300"
+      >
+        Next
+      </Button>
+    </div>
+  );
+};
+
+export default Pagination;
