3d5b1bb663dd31d964b477d67dd4568bad608954
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-16 10:01:39 +0000

    Implemented Register route and page

---
+++ app/api/register/route.ts
@@ -0,0 +1,46 @@
+import { prisma } from '@/lib';
+import bcrypt from 'bcryptjs';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, password } = await req.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: 'Email and password are required' },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return NextResponse.json(
+        { error: 'User already exists' },
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword
+      }
+    });
+
+    return NextResponse.json(
+      { message: 'User registered successfully' },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error('Registration Error:', error);
+
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
+}
---
+++ app/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from './RegisterForm';
+
+const RegisterPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-backgroundAccent">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
