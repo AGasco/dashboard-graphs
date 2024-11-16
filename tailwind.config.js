092e6455d2ff1ee6e47309b07354473f835f2a80
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-16 06:57:03 +0000

    Updated tailwindConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './app/**/*.{ts,tsx}',
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        backgroundAccent: 'var(--background-accent)',
+        foreground: 'var(--foreground)',
+        foregroundAccent: 'var(--foreground-accent)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)'
+      }
+    }
   },
   plugins: []
 };
