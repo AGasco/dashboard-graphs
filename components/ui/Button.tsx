c708429724f42517493e7d001fc436dceaca1e49
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-17 12:28:20 +0000

    Responsive: HomePage, LoginPage, RegisterPage, Navbar

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -7,7 +7,7 @@ const Button = ({ children, className, ...props }: Props) => {
   return (
     <button
       className={clsx(
-        'px-3 py-2 text-white bg-primary rounded hover:bg-blue-600',
+        'flex items-center justify-center px-3 py-2 text-white bg-primary rounded hover:bg-blue-600',
         className
       )}
       {...props}
